71aba5831a23ac129c216b8bdf992d1be0e1cac5
mario.jakobs <mario.jakobs@rb106-04.schule.local>
2014-03-31 11:21:45 +0200

    Datenbank nochmal angefangen -.-

---
+++ Sarah's Welt/src/db/DB_Beispiel.java
@@ -0,0 +1,17 @@
+package db_beispiel;
+
+/**
+ *
+ * @author Bräuer
+ */
+public class DB_Beispiel {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        new GUI().setVisible(true);
+        
+    }
+}
