24e8878f15dd0f0dd87e6d07628011b55789a03f
christmaseve <evelyn-j@arcor.de>
2014-05-05 10:07:01 +0200

    Vööögel :D

--- Sarah's Welt/src/resources/Res.java
+++ Sarah's Welt/src/resources/Res.java
@@ -20,7 +20,7 @@ public class Res {
 	public static StackedTexture BUTTERFLY  = new StackedTexture("creatures/Butterfly", 5, 2, -0.5f, -0.5f);
 	public static StackedTexture HEART = new StackedTexture("creatures/Heart", 4, 1, -0.5f, -0.2f);
 	public static StackedTexture RABBIT  = new StackedTexture("creatures/Rabbit", 5, 3, -0.5f, -0.2f);
-	public static StackedTexture BIRD  = new StackedTexture("creatures/Bird", 5, 1, -0.5f, -0.2f);
+	public static StackedTexture BIRD  = new StackedTexture("creatures/Bird", 5, 2, -0.5f, -0.2f);
 
 	public static StackedTexture SARAH = new StackedTexture("creatures/Sarah", 11, 8, -0.5f, -0.1f);
 	
